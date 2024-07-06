a1ca44c246ecac29abd6452a9906918e636d9c07
Lamardavise <dimaxxxxx800@gmail.com>
2024-07-07 00:47:20 +0300

    Generate Vapor project.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,5 +7,21 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }.description("says hello")
+    
+    app.get("hello", "vapor") { req async -> String in
+        "Hello, vapor!"
+    }
+    
+    app.get("hello", ":name") { req async -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)"
+    }
+    
+    app.get("number", ":x") { req -> String in
+        guard let int = req.parameters.get("x", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(int) is a great number"
     }
 }
